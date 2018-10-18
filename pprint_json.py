c226e87597851e849f45b90463d34e02d83f31e3
Yulia51188 <sun51188@gmail.com>
2018-10-18 18:56:59 +0300

    добавлена логика для форматирования данных json

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
-import json
-
+import json, os
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file:
+        json_data = json.loads(file.read())
+        return json_data
 
-
-def pretty_print_json(data):
-    pass
+def create_pretty_print_json(json_data):
+    pretty_json_data = json.dumps(json_data, ensure_ascii=False, sort_keys=True, indent=4)
+    return pretty_json_data
 
 
 if __name__ == '__main__':
-    pass
+    filename = input('Введите имя файла: ')
+    if os.path.exists(filename):
+        pretty_json = create_pretty_print_json(load_data(filename))
+        print(pretty_json)
+    else:
+        print('Файл не существует')
+
