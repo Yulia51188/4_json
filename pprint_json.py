46ca42869eab10c642fb8dc8bb5fe78c473ca9a2
Yulia51188 <44266591+Yulia51188@users.noreply.github.com>
2018-10-24 00:09:56 +0300

    Исправлены замечания (наименования переменных)

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,12 @@ import json, os, sys
 
 def load_data(filepath):
     with open(filepath, 'r') as file:
-        json_data = json.loads(file.read())
-        return json_data
+        yield json.loads(file.read())
 
-def create_pretty_print_json(json_data):
-    pretty_json_data = json.dumps(json_data, ensure_ascii=False, sort_keys=True, indent=4)
-    return pretty_json_data
+
+def create_formated_json(data_list):
+    formated_json_data = json.dumps(data_list, ensure_ascii=False, sort_keys=True, indent=4)
+    return formated_json_data
 
 
 if __name__ == '__main__':
@@ -17,8 +17,7 @@ if __name__ == '__main__':
         print('No argument get')
         sys.exit()
     if os.path.exists(filename):
-        pretty_json = create_pretty_print_json(load_data(filename))
-        print(pretty_json)
+        print(create_formated_json(list(load_data(filename))))
     else:
         print('File is not exists')
 
