48432c8aa5278ca1ef182814b833cde4474de688
Yulia51188 <44266591+Yulia51188@users.noreply.github.com>
2018-10-31 17:21:11 +0300

    Рефакторинг
    Разбивка на 2 функции, перехват ValueError внутри read_json_from_file, if вместо try...except в main

--- pprint_json.py
+++ pprint_json.py
@@ -1,21 +1,27 @@
 import json, os, sys
 
 
-def create_formated_json_from_file(filepath):
+def read_json_from_file(filepath):
     with open(filepath, 'r') as file:
-        python_object = json.loads(file.read())
+        str_data = file.read()
+    try:
+        python_object = json.loads(str_data)
+        return python_object
+    except ValueError:
+        return None
+
+def get_formatted_json(python_object):
     return json.dumps(python_object, ensure_ascii=False,
                       sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
-    try:
-        filename = sys.argv[1]
-    except IndexError:
+    if not len(sys.argv) > 1:
         sys.exit('No argument received')
+    filename = sys.argv[1]
     if not os.path.exists(filename):
-        sys.exit ( 'File is not exists' )
-    try:
-        print(create_formated_json_from_file(filename))
-    except ValueError:
-        sys.exit('Data in the file is not json')
+        sys.exit('File is not exists')
+    python_object = read_json_from_file(filename)
+    if python_object is None:
+        sys.exit('Data in the file is not json or file is empty')
+    print(get_formatted_json(python_object))
